dbd4b053eb6f3a53740369b5f9442a5f0b2821e8
CamiloR <camilorodriguezco7@gmail.com>
2022-07-13 00:33:12 -0500

    se instalo json-server y se creo la db donde se almacenara la informacion, se corrio json-server y se agrego la funcion fetch asyncrona para poder hacer una llamado de tipo post a la api y almacenar un cliente

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -21,8 +21,27 @@ const Formulario = () => {
                 notas:''
     })
 
-    const handleSubmit = (valores)=>{
+    const handleSubmit = async (valores)=>{
         console.log(valores);
+        try {
+            const url = "http://localhost:4000/clientes"
+
+            const respuesta = await fetch(url, {
+                method: 'POST',
+                body: JSON.stringify(valores),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+
+            })
+            console.log(respuesta);
+            const resultado = await respuesta.json()
+            console.log(resultado);
+            
+        } catch (error) {
+            console.log(error);
+        }
+
     }
 
   return (
